33e41ae67b7ff495589dfd034da0eb95071588b5
Andrei-Vlad Pisaltu-Virlan <vladvirlan.dev@gmail.com>
2025-07-31 15:43:25 +0100

    Updated Enemy Script to Stop In Front Of PlayerLight

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,17 +4,20 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] public float movementSpeed = 2f;
     Rigidbody2D rb;
+    Collider2D enemyCollider;
     [SerializeField] public Transform target;
+    [SerializeField] public Collider2D lightCollider;
     Vector2 moveDirection;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        enemyCollider = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        if (target)
+        if (target && lightCollider != null && enemyCollider != null && !lightCollider.IsTouching(enemyCollider))
         {
             Vector3 direction = (target.position - transform.position).normalized;
             moveDirection = direction;
@@ -22,6 +25,10 @@ public class Enemy : MonoBehaviour
             // Can be used to rotate the enemy when the follow:
             // float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             // rb.rotation = angle;
+        }
+        else
+        {
+            moveDirection = Vector2.zero;
         }
     }
 
