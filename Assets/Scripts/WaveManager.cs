ee132a22ab26f03b68c1c5039fa9a03cc96d0d20
ManWhoOwns5Ovens <valsandavid2006@gmail.com>
2025-07-31 16:22:55 +0100

    fixed wave bruteforce

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
@@ -5,9 +7,13 @@ public class WaveManager : MonoBehaviour
     public WaveDisplayExample waveDisplayExample;
     public WaveDisplayPlayer waveDisplayPlayer;
 
-
     public void checkSolved()
     {
+        StartCoroutine (waitCheck());
+    }
+    IEnumerator waitCheck()
+    {
+        yield return new WaitForSeconds(5.0f);
         if (waveDisplayPlayer.amplitudeSlider.value == waveDisplayExample.waveData.neededAmplitude
         && waveDisplayPlayer.wavelengthSlider.value == waveDisplayExample.waveData.neededWavelength)
         {
