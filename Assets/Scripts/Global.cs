71143036ccf5a494ef4917ced4dbdfbf6a494e64
ManWhoOwns5Ovens <valsandavid2006@gmail.com>
2025-08-01 21:08:09 +0100

    aa

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Global : MonoBehaviour
 {
-    public static int loopCounter = 1;
+    public static int loopCounter = 0;
     public static int evidenceCount = 0;
 
     public static GameObject playerInstance;
