96399ee422080aba92ac1b1da8028d26968d0fcc
Yaseen <yaseenarahman1@gmail.com>
2025-08-02 23:42:44 +0100

    a

--- Assets/Scripts/AnimalFlee.cs
+++ Assets/Scripts/AnimalFlee.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class AnimalFlee : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private float idleBeforeRunTime = 2f;
+    [SerializeField] private float fadeOutDuration = 1.5f;
 
     private Rigidbody2D rb;
     private Vector3 playerPosition;
     private Animator animator;
+    public AudioSource footsteps;
 
-    public void SetFleeTarget(Vector3 playerPos)
-    {
-        playerPosition = playerPos;
-    }
+    private bool isOffScreen = false;
+    private bool hasFadedOut = false;
 
     private void Start()
     {
@@ -22,22 +23,24 @@ public class AnimalFlee : MonoBehaviour
 
         float directionX = Mathf.Sign(transform.position.x - playerPosition.x);
 
-        // Set idle direction
         if (animator != null)
         {
             animator.SetBool("isRight", directionX < 0);
             animator.SetBool("isMoving", false);
         }
 
-        // Start flee after idle animation
         StartCoroutine(FleeAfterDelay(directionX));
     }
 
-    private System.Collections.IEnumerator FleeAfterDelay(float directionX)
+    public void SetFleeTarget(Vector3 playerPos)
+    {
+        playerPosition = playerPos;
+    }
+
+    private IEnumerator FleeAfterDelay(float directionX)
     {
         yield return new WaitForSeconds(idleBeforeRunTime);
 
-        // Set movement direction and start running
         Vector2 fleeDirection = new Vector2(directionX, 0f);
         rb.linearVelocity = fleeDirection * speed;
 
@@ -46,10 +49,43 @@ public class AnimalFlee : MonoBehaviour
             animator.SetBool("isMoving", true);
             animator.SetBool("isRight", directionX > 0);
         }
+
+        if (footsteps != null)
+        {
+            footsteps.volume = 1f;
+            footsteps.Play();
+            StartCoroutine(FadeFootsteps());
+        }
+    }
+
+    private IEnumerator FadeFootsteps()
+    {
+        float startVolume = footsteps.volume;
+        float timeElapsed = 0f;
+
+        while (timeElapsed < fadeOutDuration)
+        {
+            footsteps.volume = Mathf.Lerp(startVolume, 0f, timeElapsed / fadeOutDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        footsteps.Stop();
+        hasFadedOut = true;
+
+        if (isOffScreen)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnBecameInvisible()
     {
-        Destroy(gameObject);
+        isOffScreen = true;
+
+        if (hasFadedOut)
+        {
+            Destroy(gameObject);
+        }
     }
 }
