30ba71f9d12bf8a71121634995e8907d32ac3d67
Yaseen <yaseenarahman1@gmail.com>
2025-08-03 05:37:37 +0100

    a

---
+++ Assets/Scripts/Lighting/Footprints.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Footprints : MonoBehaviour
+{
+    public Collider2D flashlightCollider; // Assign the flashlight collider in Inspector
+    private SpriteRenderer spriteRenderer;
+    private Collider2D ownCollider;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ownCollider = GetComponent<Collider2D>();
+
+        if (spriteRenderer == null || ownCollider == null)
+        {
+            Debug.LogError("Missing SpriteRenderer or Collider2D on " + gameObject.name);
+        }
+    }
+
+    void Update()
+    {
+        if (flashlightCollider != null && ownCollider != null)
+        {
+            bool isLit = flashlightCollider.IsTouching(ownCollider);
+            spriteRenderer.enabled = isLit;
+        }
+    }
+}
